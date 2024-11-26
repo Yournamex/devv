f4230df013015be3286d150cd5a9f012a5e79c09
Yournamex <127647931+Yournamex@users.noreply.github.com>
2024-11-26 08:01:38 +0700

    Add files via upload

---
+++ Dev/Testt/script.js
@@ -0,0 +1,18 @@
+document.write("<h1>Gu Ja Hack Meung!!!</h1>");
+document.write("<p>ก็มาดิวะ</p>");
+
+alert("You Are Gay");
+
+x = 10.5623
+z = 15.22
+y = x + z
+console.log(y)
+console.error("อ่านทำไมจ้ะ")
+console.warn("มีอะไรผิดปกตินะจ้ะ")
+
+var a =10,b=20,c=30,d=40
+a = 100
+document.write(a + " " + b + " " + c + " " +d)
+
+name = "Paranyu" , age = 18;
+console.log(name + " " + "อายุ =",age)
